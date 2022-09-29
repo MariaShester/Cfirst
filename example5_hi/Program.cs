c267d4be958658465d0be343f7000deb2da43df1
Maria <shesterikovamasha@gmail.com>
2022-09-29 19:08:24 +0300

    Added my name

---
+++ example5_hi/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Write your name: ");
+string username = Console.ReadLine(); 
+if(username == Maria)
+{
+        Console.WriteLine("Hello, Mariaaa");
+}
+else
+{
+        Console.Write("Who are you, ");
+        Console.WriteLine("username");
+}
